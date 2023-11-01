9c7d74bb589910f97460f9ad6c9e09923cfce5ca
navneetguptacse <navneetguptaid7@gmail.com>
2023-11-01 13:33:00 +0530

    Update

---
+++ src/home/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const title = (
+    <h2>Search your One From Thousand of Products</h2>
+)
+
+const desc = "We have the largest collection of products"
+
+const Banner = () => {
+  return (
+    <div>Banner</div>
+  )
+}
+
+export default Banner
